269737a912d50725124730c3fe17aad22f21a7c8
karolsudol <karol@sudoltrust.com>
2023-12-21 16:54:56 +0000

    dogbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from 'solid-js';
+import './App.css';
+import dogImage from './assets/dog.png';
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+        <a href="https://solana.fm/address/HexkqtFMmBJXUHco9KoXKocQcLSgfCf2Hdz1Qa3jcSEA/transactions?cluster=mainnet-alpha" target="_blank">
+          <img src={dogImage} class="logo solid" alt="Dog image" style={{ width: '200px' }} />
         </a>
       </div>
-      <h1>Vite + Solid</h1>
+      <h1>DOGBOT</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+        site in progress
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
