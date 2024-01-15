dd441cd0e182fa6f54e31c69b2d2b06673fdbd07
karolsudol <karol@sudoltrust.com>
2024-01-15 18:07:38 +0000

    rpc scripts

---
+++ src/scripts/rpc/getFungibleAssetsByOwner.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+// Configure dotenv to load the .env file
+dotenv.config();
+
+const url = `https://mainnet.helius-rpc.com/?api-key=${process.env.VITE_API_KEY}`;
+
+const searchAssets = async () => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      id: 'my-id',
+      method: 'searchAssets',
+      params: {
+        ownerAddress: 'HexkqtFMmBJXUHco9KoXKocQcLSgfCf2Hdz1Qa3jcSEA',
+        tokenType: 'fungible',
+      },
+    }),
+  });
+  const { result } = await response.json();
+  console.log('Search Assets: ', result);
+};
+searchAssets();
