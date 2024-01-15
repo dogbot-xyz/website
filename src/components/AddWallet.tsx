57f1a271fda58d17373a4fb3d8ca0413fcba45d0
karolsudol <karol@sudoltrust.com>
2024-01-15 14:16:10 +0000

    pretteir and lint config

--- src/components/AddWallet.tsx
+++ src/components/AddWallet.tsx
@@ -1,18 +1,18 @@
-import { createSignal, Setter, JSX } from "solid-js";
-import { Wallet } from "../App";
+import { createSignal, Setter, JSX } from 'solid-js';
+import { Wallet } from '../App';
 
 export interface AddWalletProps {
   setWallets: Setter<Wallet[]>;
 }
 
-const emptyWallet: Wallet = { title: "", address: "" };
-
+const emptyWallet: Wallet = { title: '', address: '' };
 
 export function AddWallet(props: AddWalletProps) {
-
   const [newWallet, setNewWallet] = createSignal(emptyWallet);
 
-  const addWallet: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
+  const addWallet: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (
+    event
+  ) => {
     event.preventDefault();
     props.setWallets((wallets) => [...wallets, newWallet()]);
     setNewWallet(emptyWallet);
