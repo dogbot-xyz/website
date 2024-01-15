ec1083216c6352932535ff34918680119d9dca62
karolsudol <karol@sudoltrust.com>
2024-01-15 12:18:37 +0000

    reactive setter and singals done

--- src/components/AddWallet.tsx
+++ src/components/AddWallet.tsx
@@ -1,26 +1,47 @@
-import { Setter, JSX } from "solid-js";
+import { createSignal, Setter, JSX } from "solid-js";
 import { Wallet } from "../App";
 
 export interface AddWalletProps {
   setWallets: Setter<Wallet[]>;
 }
+
+const emptyWallet: Wallet = { title: "", address: "" };
+
+
 export function AddWallet(props: AddWalletProps) {
+
+  const [newWallet, setNewWallet] = createSignal(emptyWallet);
+
   const addWallet: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
     event.preventDefault();
-    props.setWallets([]);
+    props.setWallets((wallets) => [...wallets, newWallet()]);
+    setNewWallet(emptyWallet);
   };
+
   return (
     <form>
       <div>
-        <label for="title">wallet name</label>
-        <input id="title" />
+        <label for="title">Wallet name</label>
+        <input
+          id="title"
+          value={newWallet().title}
+          onInput={(e) => {
+            setNewWallet({ ...newWallet(), title: e.currentTarget.value });
+          }}
+        />
       </div>
       <div>
         <label for="address">Address</label>
-        <input id="address" />
+        <input
+          id="author"
+          value={newWallet().address}
+          onInput={(e) => {
+            setNewWallet({ ...newWallet(), address: e.currentTarget.value });
+          }}
+        />
       </div>
       <button type="submit" onClick={addWallet}>
-        Add wallet
+        Add book
       </button>
     </form>
   );
